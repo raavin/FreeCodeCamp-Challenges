cd40a3849a7340be4cec9b47abe0510c75bf0bb4
Jason Keenan <jason.keenan@raavin.com>
2016-06-04 22:22:00 +1000

    Create boo-who.js

---
+++ boo-who.js
@@ -0,0 +1,13 @@
+
+function booWho(bool) {
+  if (bool === true || bool === false){
+    return true;
+  } else {
+    return false;
+  }
+  // What is the new fad diet for ghost developers? The Boolean.
+  //return console.log(2);
+  //return bool;
+}
+
+booWho(null);
