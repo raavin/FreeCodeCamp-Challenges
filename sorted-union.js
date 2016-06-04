517b7812f46b10dcfc9a28318093651fc841a278
Jason Keenan <jason.keenan@raavin.com>
2016-06-04 22:53:30 +1000

    Some not passing

--- sorted-union.js
+++ sorted-union.js
@@ -6,7 +6,12 @@ function uniteUnique(arr) {
     newArr.push(arguments[i]); //push arguments to newArr
   }
   newArr = [].concat.apply([], newArr);// flatten newArr
-  return newArr;
+  var uniqueArray = newArr.filter(function(elem, pos, arr) {
+    return arr.indexOf(elem) == pos;
+  });
+    
+  //}
+  return uniqueArray;
 }
 
 uniteUnique([1, 3, 2], [5, 2, 1, 4], [2, 1]);
