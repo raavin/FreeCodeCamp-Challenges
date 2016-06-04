3139a687c79c6b9cca6a30d8dd195e4d2acad309
Jason Keenan <jason.keenan@raavin.com>
2016-06-05 08:36:33 +1000

    passing tests

--- sorted-union.js
+++ sorted-union.js
@@ -2,10 +2,12 @@
 function uniteUnique(arr) {
   
   var newArr = [];
-  for (var i = 0; i < arr.length; i++){
+  for (var i = 0; i < arguments.length; i++){
     newArr.push(arguments[i]); //push arguments to newArr
   }
+  console.log(newArr);
   newArr = [].concat.apply([], newArr);// flatten newArr
+  //console.log(newArr);
   var uniqueArray = newArr.filter(function(elem, pos, arr) {
     return arr.indexOf(elem) == pos;
   });
@@ -14,4 +16,6 @@ function uniteUnique(arr) {
   return uniqueArray;
 }
 
-uniteUnique([1, 3, 2], [5, 2, 1, 4], [2, 1]);
+//uniteUnique([1, 3, 2], [5, 2, 1, 4], [2, 1]);
+//uniteUnique([1, 2, 3], [5, 2, 1]);
+//uniteUnique([1, 2, 3], [5, 2, 1, 4], [2, 1], [6, 7, 8]);
