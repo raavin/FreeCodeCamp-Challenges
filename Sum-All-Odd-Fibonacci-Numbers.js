4b560d13d9a460d935e208db089af6cb0b5f8cad
Jason Keenan <jason.keenan@raavin.com>
2016-06-05 21:23:52 +1000

    Update Sum-All-Odd-Fibonacci-Numbers.js

--- Sum-All-Odd-Fibonacci-Numbers.js
+++ Sum-All-Odd-Fibonacci-Numbers.js
@@ -2,16 +2,20 @@
 function sumFibs(num) {
   var duo = [1,1];
   var nextNumber = 0;
-  var totalOdd = 0;
-  for (var i = 0; i < num; i++){
+  var totalOdd = 1;
+  for (var i = 0; totalOdd < num; i++){
     nextNumber = duo[0] + duo[1];
+    console.log(nextNumber);
     if (nextNumber%2 !== 0){
       totalOdd = totalOdd + nextNumber;
     }
     duo.push(nextNumber);
     duo.shift();
   }
-  return totalOdd;
+  console.log(duo);
+  console.log(totalOdd);
+  return totalOdd + 1;
 }
 
 sumFibs(4);
+sumFibs(75024);
