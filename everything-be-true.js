08c3c58e47e2c961c59f2ec5cc5877a77aa39089
Jason Keenan <jason.keenan@raavin.com>
2016-06-07 21:40:39 +1000

    Update everything-be-true.js

--- everything-be-true.js
+++ everything-be-true.js
@@ -1,37 +1,26 @@
 
 function truthCheck(collection, pre) {
   // Is everyone being true?
-  var userDetails = collection[3];
-  //return collection[0].hasOwnProperty(pre);
-  if (userDetails.hasOwnProperty(pre) || userDetails[pre] == 0 ? false : true){
-    return false;
-  } else {
-    return true;
-  }
+  
+   var result = true;
+   for (var i = 0; i < collection.length; i++){
+     var userDetails = collection[i];
+     if (userDetails.hasOwnProperty(pre) || 
+         !userDetails[pre] === 0 ||
+         !userDetails[pre] === null ||
+         isNaN(!userDetails[pre])){
+        result = false;
+      } 
+    }
+   return result;
 }
 
-truthCheck([{
-    "user": "Tinky-Winky",
-    "sex": "male"
-}, {
-    "user": "Dipsy",
-    "sex": "male"
-}, {
-    "user": "Laa-Laa",
-    "sex": "female"
-}, {
-    "user": "Po",
-    "sex": "female"
-}], "sex");
-truthCheck([{
-    "user": "Tinky-Winky",
-    "sex": "male"
-}, {
-    "user": "Dipsy"
-}, {
-    "user": "Laa-Laa",
-    "sex": "female"
-}, {
-    "user": "Po",
-    "sex": "female"
-}], "sex");
+//truthCheck([{"user": "Tinky-Winky", "sex": "male"}, {"user": "Dipsy", "sex": "male"}, {"user": "Laa-Laa", "sex": "female"}, {"user": "Po", "sex": "female"}], "sex");// should return true.
+//truthCheck([{"user": "Tinky-Winky", "sex": "male"}, {"user": "Dipsy"}, {"user": "Laa-Laa", "sex": "female"}, {"user": "Po", "sex": "female"}], "sex");// should return false.
+//truthCheck([{"user": "Tinky-Winky", "sex": "male", "age": 0}, {"user": "Dipsy", "sex": "male", "age": 3}, {"user": "Laa-Laa", "sex": "female", "age": 5}, {"user": "Po", "sex": "female", "age": 4}], "age");// should return false.
+//truthCheck([{"name": "Pete", "onBoat": true}, {"name": "Repeat", "onBoat": true}, {"name": "FastFoward", "onBoat": null}], "onBoat");// should return false
+//truthCheck([{"name": "Pete", "onBoat": true}, {"name": "Repeat", "onBoat": true, "alias": "Repete"}, {"name": "FastFoward", "onBoat": true}], "onBoat");// should return true
+//truthCheck([{"single": "yes"}], "single");// should return true
+//truthCheck([{"single": ""}, {"single": "double"}], "single");// should return false
+//truthCheck([{"single": "double"}, {"single": undefined}], "single");// should return false
+//truthCheck([{"single": "double"}, {"single": NaN}], "single");// should return false
