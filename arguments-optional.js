b9fb5d28f21f048554700b81fa2e4aa635f09fec
Jason Keenan <jason.keenan@raavin.com>
2016-06-08 23:31:12 +1000

    Update arguments-optional.js

--- arguments-optional.js
+++ arguments-optional.js
@@ -1,15 +1,22 @@
-
 function addTogether() {
-  var outerArg = arguments[0];
-  if (arguments.length == 2){
-      return arguments[0] + arguments[1];
-  } else {
-    return function(innerArg) {
-      return outerArg + innerArg;
-  };
-  }
-}
-var sumTwoAnd = addTogether();
-//addTogether(2,3);
-addTogether(1)(4);
-
+        var outerArg = arguments[0];
+        if (isNaN(outerArg)) {
+            return undefined;
+        } else {
+            if (arguments.length == 2) {
+                return arguments[0] + arguments[1];
+            } else {
+                return function(innerArg) {
+                    if (isNaN(innerArg)) {
+                        return undefined;
+                    } else {
+                        return outerArg + innerArg;
+                    }
+                };
+            }
+        }
+    }
+    //var sumTwoAnd = addTogether();
+    //addTogether(2,3);
+    //addTogether(1)(4);
+addTogether("http://bit.ly/IqT6zt");
